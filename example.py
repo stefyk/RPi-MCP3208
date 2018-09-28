767a733f1e1763333382666a67f8552e3905b51e
stefyk <43543157+stefyk@users.noreply.github.com>
2018-09-28 11:21:37 +0000

    Update example.py

--- example.py
+++ example.py
@@ -10,18 +10,18 @@ spi_channel = 0
 spi = spidev.SpiDev(0, spi_channel)
 spi.max_speed_hz = 1000000
 
-# Hardware SPI configuration:
+# Hardware SPI configuration (wiring explained in README file):
 SPI_PORT   = 0
 SPI_DEVICE = 0
-	
 
 def read_adc(adc_channel, Vref = 3.3):
 	adc_channel = 0
 			
-	msg = 0b11
-	msg = ((msg << 1) + adc_channel) << 5
-	msg = [msg, 0b00000000]
-	reply = spi.xfer2(msg)
+	data = 0b11
+	data = ((data << 1) + adc_channel) << 5
+	data = [data, 0b00000000]
+	#Performs the SPI transaction and assigns the data to "reply"
+	reply = spi.xfer2(data)
 	
 	# Construct single integer out of the reply (2 bytes)
 	adc = 0
@@ -39,9 +39,9 @@ try:
 	while True:
 		# The read_adc function will get the value of the specified channel (0-1).
 		adc_0 = read_adc(0)
-		adc_1 = read_adc(1)
 		# Print the ADC values.
 		print("The amplitude of V from Ch.0 is:", round(adc_0, 2),"V")
 		time.sleep(0.5)
 finally:
+	#closing the SPI channel
 	close()
