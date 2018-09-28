ba81d99ed2ce02e5b2d5d686378bba636d9f2374
stefyk <43543157+stefyk@users.noreply.github.com>
2018-09-28 12:00:14 +0000

    Update example.py

--- example.py
+++ example.py
@@ -18,7 +18,7 @@ def read_adc(adc_channel, Vref = 3.3):
 	adc_channel = 0
 			
 	data = 0b11
-	data = ((data << 1) + adc_channel) << 5
+	data = ((data << 1) + adc_channel) << 7
 	data = [data, 0b0000000000]
 	#Performs the SPI transaction and assigns the data to "reply"
 	reply = spi.xfer2(data)
@@ -29,7 +29,7 @@ def read_adc(adc_channel, Vref = 3.3):
 		adc = (adc << 10) + n
 
 	# Last bit (0) is not part of ADC value, shift to remove it
-	adc = adc >> 1
+	adc = adc >> 3
 
 	# Calculate voltage form ADC value
 	Voltage = (Vref * adc) / 4096
