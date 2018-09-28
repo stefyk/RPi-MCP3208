f7032195e1fc1602a0d264308dadeeaa669023c0
stefyk <43543157+stefyk@users.noreply.github.com>
2018-09-28 11:02:13 +0000

    Update example.py

--- example.py
+++ example.py
@@ -32,7 +32,7 @@ def read_adc(adc_channel, Vref = 3.3):
 	adc = adc >> 1
 
 	# Calculate voltage form ADC value
-	Voltage = (3.3 * adc) / 1024
+	Voltage = (Vref * adc) / 4096
         
 	return Voltage
 try:			
